0cf42547e738e5bf422111efc1dac99fc4a8dcbc
yishin0819 <yishin0819@gmail.com>
2017-07-01 22:55:13 +0800

    second

--- R/big2utf8.R
+++ R/big2utf8.R
@@ -1,11 +1,12 @@
 #' convert file format from big5 to utf8
 #'
 #' This function allows you to covert file from big5 to utf8.
-#' @param 
+#' @param filename This is source file.
+#' @param newfile This is destination file.
 #' @keywords big5
 #' @export
 #' @examples
-#' big2utf8(sourcefile,newfile)
+#' big2utf8('sourcefile.txt','newfile.csv')
 big2utf8 <- function(filename,newfile){
   data <- readLines(filename, encoding="big5")
   data <- iconv(data, "big5", "utf8", sub="?")
